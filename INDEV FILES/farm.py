59e4a4795993a8aa4c10c1374f85f0671035ed32
Lillian Bailey <d979b438@home.ku.edu>
2026-09-08 14:47:32 -0500

    Pushing further work to farm.py

--- INDEV FILES/farm.py
+++ INDEV FILES/farm.py
@@ -17,4 +17,36 @@ class Farm:
         self.bankrupt = bankrupt
         self.total_money = totalmoney
         self.crops_harvested = cropsharvested
+        self.seasonlen = 15
+        self.seasons = ["Spring", "Summer", "Fall", "Winter"]
+
+    def display_status(self):
+        print(f"You stand in the middle of {self.name} farm.")
+        print(f"It is Year {self.years}, Season {self.seasons[self.season_index]}, Day {self.season_day}.")
+        print(f"You have {self.money} in your wallet.")
+
+    def add_money(self, amount):
+        self.money += amount
+    
+    def remove_money(self ,amount):
+        if self.money - amount > 0:
+            self.money -= amount
+            return True
+        else:
+            return False
         
+    def advance_day(self):
+        self.days += 1
+        self.season_day += 1
+        if self.season_day > self.seasonlen:
+            self.seasonday = 1
+            self.season_index = (self.season_index + 1) % len(self.seasons)
+            if self.season_index == 0:
+                years += 1
+    
+    def check_bankruptcy(self):
+        if self.money <= 0:
+            self.bankrupt = True
+            return True
+        else:
+            return False
