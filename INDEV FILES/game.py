f66ecc02e6b4680ad80361d5f488ce883a4d3022
Lillian Bailey <d979b438@home.ku.edu>
2026-09-10 14:12:53 -0500

    Pushing base of game.py

---
+++ INDEV FILES/game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self, farm, shop):
+        self.farm = farm
+        self.shop = shop
+        self.running = True
+
+    def run(self):
+        #Big Game Loop
+        pass
+
+    def display_menu(self):
+        pass
